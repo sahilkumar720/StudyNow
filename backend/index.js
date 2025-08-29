725956a1037ed883e43220e24b33d7cf8bad4593
sahilkumar720 <110218150+sahilkumar720@users.noreply.github.com>
2025-08-29 13:36:37 +0530

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ let app = express()
 app.use(express.json())
 app.use(cookieParser())
 app.use(cors({
-    origin:"http://localhost:5173",
+    origin:"https://studynow1.onrender.com",
     credentials:true
 }))
 app.use("/api/auth", authRouter)
